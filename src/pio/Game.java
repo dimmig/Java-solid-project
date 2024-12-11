f75af85c908fab76748d3ad862e098dc2ce7356d
D. Galagan <dima@airtrail.co>
2024-12-11 09:57:31 +0100

    added stats

--- src/pio/Game.java
+++ src/pio/Game.java
@@ -1,11 +1,13 @@
 package pio;
 
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.Random;
 
 public class Game {
     private final ArrayList<Player> players = new ArrayList<>();
     private final Random rand = new Random();
+    private final HashMap<Player, Integer> stats = new HashMap<>();
 
     public void addPlayer(Player player) {
         if (!players.contains(player)) {
@@ -43,6 +45,11 @@ public class Game {
                     i++;
                 }
             } else {
+                if (!stats.containsKey(players.get(i))) {
+                    stats.put(players.get(i), 1);
+                } else {
+                    stats.put(players.get(i), stats.get(players.get(i)) + 1);
+                }
                 System.out.println("PLayer " + this.players.get(i).getName() + " won!");
                 return;
             }
@@ -52,6 +59,14 @@ public class Game {
     void printPlayers() {
         for (Player player : players) {
             System.out.println(player.toString());
+        }
+    }
+
+    void printStats() {
+        System.out.println("-------------------");
+        System.out.println("Statistics:");
+        for (var entry : stats.entrySet()) {
+            System.out.println(entry.getKey().getName() + ": " + entry.getValue());
         }
     }
 }
--- src/pio/Main.java
+++ src/pio/Main.java
@@ -6,15 +6,19 @@ public class Main {
         Player player = new PlayerComputer("zzz");
         Player player1 = new PlayerComputer("1111");
         Player player2 = new PlayerComputer("222");
-        Player player3 = new PlayerHuman("human");
+//        Player player3 = new PlayerHuman("human");
         Game game = new Game();
         game.addPlayer(player);
         game.addPlayer(player1);
         game.addPlayer(player2);
-        game.addPlayer(player3);
-        game.play();
+//        game.addPlayer(player3);
+        for (int i = 0; i < 10; i++) {
+            game.play();
+        }
         game.removePlayer("222");
         game.printPlayers();
+
+        game.printStats();
     }
 
 }
